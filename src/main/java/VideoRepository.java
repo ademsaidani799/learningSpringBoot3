3f011e9a9142d3d9f332bcedc8088b6540d76758
adem saidani <ademsaidani799@gmail.com>
2023-07-12 18:26:03 +0200

    adding the search form

--- src/main/java/VideoRepository.java
+++ src/main/java/VideoRepository.java
@@ -1,5 +1,9 @@
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VideoRepository extends JpaRepository
         <VideoEntity, Long> {
+    List<VideoEntity> findByName (String name);
+
 }
